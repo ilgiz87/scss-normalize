aab3b1f16943b37bcbcee5cc14d30909a26fef5e
ILGIZ <ilgiz-87@mail.ru>
2025-02-01 18:31:32 +0500

    v 1.0.0

--- postcss.config.js
+++ postcss.config.js
@@ -1,8 +1,12 @@
 module.exports = {
   plugins: [
-    require('postcss-combine-media-query')(),
+    require('postcss-preset-env')({
+      stage: 1, // Уровень экспериментальных функций (0-4)
+      browsers: ['last 3 versions', '> 1%', 'not dead'], // Поддерживаемые браузеры
+    }),
+    require('postcss-combine-media-query')(), // Объединение медиазапросов
     require('cssnano')({
-      preset: 'default',
+      preset: 'default', // Минификация CSS
     }),
   ],
 };
